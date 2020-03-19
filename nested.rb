b932f5321b3fa881adac639e2320cdaf3c6a866e
George Chen <georgechen1338@gmail.com>
2020-03-19 18:49:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-  programmer_hash[:yukihiro_matsumoto] = ""
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "COBOL"}
 end
 
 def changing_alan
