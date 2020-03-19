12877cdf2e484a22ce198d76933e2c0bdc06b1fb
George Chen <georgechen1338@gmail.com>
2020-03-19 18:51:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
-  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"}
-  programmer_hash{:yukihiro_matsumoto] = {:languages => ["LISP"]}
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"},
+  programmer_hash{:yukihiro_matsumoto] = {:languages => ["LISP", "C"]}
 end
 
 def changing_alan
